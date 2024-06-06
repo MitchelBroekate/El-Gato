8c853b9a80dd38b35281e1306d5903546d378917
MitchelBroekate <mitchelb11@outlook.com>
2024-06-06 23:48:39 +0200

    Missile almost done

--- El gato/Assets/Scripts/Towers/CornTower.cs
+++ El gato/Assets/Scripts/Towers/CornTower.cs
@@ -1,21 +1,154 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class CornTower : TowerManager
 {
+    #region Variables
+    [Header("Children Of GameObject")]
+    [SerializeField]
+    Transform missileSpawn;
+    [SerializeField]
+    GameObject missileCorn;
+
+    [Header("List With Targets")]
+    public List<Transform> allTargets = new();
+
+    [Header("Current Targeted Target")]
+    public Transform nearestTarget;
+    bool missileSpeedIncrease = true;
+
+    GameObject missile;
+
+    bool speedstop = true;
+
+    #endregion
 
     [System.Obsolete]
     private void Start()
     {
+        missileSpawn = transform.FindChild("MissileSpawn");
+        missileCorn = transform.FindChild("Missile").gameObject;
         rangeScale = 30;
-        health = 300;
+        health = 150;
+        bulletSpeed = 10;
+
+        fireRate = 10;
+
+        GetComponent<SphereCollider>().radius = rangeScale;
     }
 
     private void Update()
     {
-        GetComponent<SphereCollider>().radius = rangeScale;
+        Targeting();
+
+        DeathCheck();
     }
 
+    private void FixedUpdate()
+    {
+        if (missile != null)
+        {
+            if (missileSpeedIncrease)
+            {
+                if (speedstop)
+                {
+                    StartCoroutine(BulletSpeedStop(3));
+                    speedstop = false;
+                }
+                bulletSpeed += 0.1f;
+                missile.GetComponent<Rigidbody>().AddForce(transform.up * bulletSpeed);
+            }
+        }
+   
+    }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.gameObject.tag == "enemyship")
+        {
+            if (!allTargets.Contains(other.transform))
+            {
+                allTargets.Add(other.transform);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.transform.gameObject.tag == "enemyship")
+        {
+            if (allTargets.Contains(other.transform))
+            {
+                allTargets.Remove(other.transform);
+
+                if (other.transform == nearestTarget)
+                {
+                    nearestTarget = null;
+                }
+            }
+        }
+    }
+
+    void Targeting()
+    {
+        float distance;
+        float nearestDistance = float.MaxValue;
+
+        for (int i = 0; i < allTargets.Count; i++)
+        {
+            if (allTargets[i] != null)
+            {
+                distance = Vector3.Distance(transform.position, allTargets[i].position);
+
+                if (distance < nearestDistance)
+                {
+                    nearestTarget = allTargets[i];
+                    nearestDistance = distance;
+
+                }
+            }
+        }
+        if (nearestTarget != null)
+        {
+            if (Time.time >= whenToFire)
+            {
+                whenToFire = Time.time + 1 * fireRate;
+                Shooting();
+            }
+        }
+    }
+
+    void Shooting()
+    {
+        missile = Instantiate(missileCorn, missileSpawn.position, quaternion.identity);
+        missile.transform.parent = transform;
+        speedstop = true;
+        missileSpeedIncrease = true;
+
+        missile.SetActive(true);
+    }
+
+    void DeathCheck()
+    {
+        if (nearestTarget != null)
+        {
+            if (nearestTarget.GetComponent<UfoBehavior>().dead)
+            {
+                allTargets.Remove(nearestTarget);
+                Destroy(nearestTarget.gameObject);
+
+                GameObject.Find("Scripts/PlayerInput").GetComponent<BuildingShop>().money += 50;
+            }
+        }
+
+    }
+
+    IEnumerator BulletSpeedStop(float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        missileSpeedIncrease = false;
+    }
 }
--- El gato/Assets/Scripts/Towers/bullet/CornBulletBehavior.cs
+++ El gato/Assets/Scripts/Towers/bullet/CornBulletBehavior.cs
@@ -1,27 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
+using TreeEditor;
+using Unity.Mathematics;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CornBulletBehavior : BulletManager
 {
+
+    float moveSpeed = 1500;
+    Transform target;
+    Quaternion targetRotation;
     private void Start()
     {
         bulletDamage = 300;
+
+        target = transform.parent.GetComponent<CornTower>().nearestTarget;
+        targetRotation = Quaternion.Euler(target.position.x, target.position.y, target.position.z);
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.transform.tag == "enemyship")
-        {
-            collision.gameObject.GetComponent<UfoBehavior>().health -= bulletDamage;
-        }
+            if (collision.transform.tag == "enemyship")
+            {
+                collision.gameObject.GetComponent<UfoBehavior>().health -= bulletDamage;
+            }
 
-        Destroy(gameObject);
-
+            Destroy(gameObject);
     }
+
+    //private void OnTriggerStay(Collider other)
+    //{
+    //    if (other.transform.tag == "enemyship")
+    //    {
+    //        Destroy(gameObject);
+    //    }
+    //}
 
     private void Update()
     {
-        Destroy(gameObject, 5);
+            Destroy(gameObject, 50);
+
     }
+
+    //private void FixedUpdate()
+    //{
+    //    if (start)
+    //    {
+    //        transform.GetComponent<Rigidbody>().velocity = transform.forward * moveSpeed;
+
+    //        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, 1f);
+    //    }
+    //}
 }
