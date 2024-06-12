5e3873ad43453a0d775363f07f976d4f2cda6f57
MitchelBroekate <mitchelb11@outlook.com>
2024-06-12 16:42:40 +0200

    Alien + Dropship Setup

--- El gato/Assets/Scripts/Aliens/DropshipBehaviour.cs
+++ El gato/Assets/Scripts/Aliens/DropshipBehaviour.cs
@@ -4,25 +4,62 @@ using UnityEngine;
 
 public class DropshipBehaviour : MonoBehaviour
 {
-    float decendSpeed;
+    float decendSpeedShip;
+    float decendSpeedAlien;
+
+    float ascendSpeedShip;
+
+    RaycastHit hit;
 
     shipStates currentState;
+
+    GameObject alien;
+    GameObject currentAlien;
+
+    Transform spawnLocation;
+
+    List<Transform> spawnpoints = new();
+
+    bool doRoutine = true;
+
     enum shipStates
     {
         DECENDING,
         DROPOFF,
         SHIPEXIT    
     }
+    [System.Obsolete]
     private void Start()
     {
-        decendSpeed = 100;
+        GetShipChildInfo();
 
+        decendSpeedShip = 1000;
+        decendSpeedAlien = 10;
+
+        ascendSpeedShip = 10;
+
+        currentState = shipStates.DECENDING;
 
     }
     private void Update()
     {
         DoState();
         StateSwitcher();
+    }
+
+    [System.Obsolete]
+    void GetShipChildInfo()
+    {
+        alien = transform.FindChild("Alien").gameObject;
+        spawnLocation = transform.FindChild("Spawns").transform;
+
+        for (int i = 0; 0 < spawnLocation.childCount -1; i++) 
+        {
+            if (!spawnpoints.Contains(spawnLocation.GetChild(i)))
+            {
+                spawnpoints.Add(spawnLocation.GetChild(i));
+            }
+        }
     }
 
     void DoState()
@@ -50,17 +87,73 @@ public class DropshipBehaviour : MonoBehaviour
 
     void Decending()
     {
+        GetComponent<Rigidbody>().velocity = -transform.up * decendSpeedShip * Time.deltaTime;
+
+        if (Physics.Raycast(transform.position, -transform.up, out hit, Mathf.Infinity))
+        {
+
+            if (Vector3.Distance(transform.position, hit.point) <= 60)
+            {
+                GetComponent<Rigidbody>().AddForce(transform.up * ascendSpeedShip * Time.deltaTime);
+            }
+
+            if (Vector3.Distance(transform.position, hit.point) >= 200)
+            {
+                Destroy(gameObject);
+            }
+        }
 
     }
 
     void DropOff()
     {
-
+        if (doRoutine)
+        {
+            StartCoroutine(AlienSpawn());
+        }
     }
 
     void ShipExit()
     {
+        GetComponent<Rigidbody>().velocity = transform.up * decendSpeedShip * Time.deltaTime;
 
+        if (Physics.Raycast(transform.position, -transform.up, out hit, Mathf.Infinity))
+        {
+            if (Vector3.Distance(transform.position, hit.point) <= 60)
+            {
+                if (decendSpeedShip > 0)
+                {
+                    decendSpeedShip -= 5;
+                }
+
+            }
+
+            if (Vector3.Distance(transform.position, hit.point) <= 30)
+            {
+                GetComponent<Rigidbody>().velocity = Vector3.zero;
+            }
+        }
+    }
+
+    IEnumerator AlienSpawn()
+    {
+        doRoutine = false;
+
+        for (int i = 0; 0 < Random.Range(2, 6); i++)
+        {
+            int randomWaitTime = Random.Range(4, 6);
+            int randomSpawn = Random.Range(0, 4);
+
+            currentAlien = Instantiate(alien, spawnpoints[randomSpawn].position, Quaternion.identity);
+            currentAlien.SetActive(true);
+            currentAlien.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+
+            currentAlien.GetComponent<Rigidbody>().velocity = -currentAlien.transform.up * decendSpeedAlien * Time.deltaTime;
+
+            yield return new WaitForSeconds(randomWaitTime);
+        }
+
+        currentState = shipStates.SHIPEXIT;
     }
 
 }
