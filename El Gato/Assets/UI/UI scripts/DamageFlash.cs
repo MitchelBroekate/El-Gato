ae76f4f098c58f6543e632a5b131038473111b02
MacKenzie1976 <97097945@st.deltion.nl>
2024-06-14 16:23:22 +0200

    Particles

---
+++ El Gato/Assets/UI/UI scripts/DamageFlash.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageFlash : MonoBehaviour
+{
+    public MeshRenderer meshRenderer;
+    public Color origColour;
+    public float flashTime = .15f;
+
+
+    public void FlashStart()
+    {
+        
+    }
+
+    public void FlashEnd()
+    {
+
+    }
+}
